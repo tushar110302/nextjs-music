b744f63de289552451fd3168e00526227ac78d88
Tushar Ranjan <thrrnjn1103@gmail.com>
2025-03-26 08:56:18 +0530

    Add components and UI elements to music school website

---
+++ src/app/courses/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Courses() {
+  return (
+    <div>page2</div>
+  )
+}
+
+export default Courses
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,9 @@
 import FeaturedCourses from "@/components/FeaturedCourses";
+import Footer from "@/components/Footer";
 import HeroSection from "@/components/HeroSection";
+import Instructors from "@/components/Instructors";
+import TestimonialCards from "@/components/TestimonialCards";
+import UpcomingWebinars from "@/components/ui/UpcomingWebinars";
 import WhyChooseUs from "@/components/WhyChooseUs";
 
 
@@ -10,6 +14,10 @@ export default function Home() {
       <HeroSection />
       <FeaturedCourses/>
       <WhyChooseUs/>
+      <TestimonialCards/>
+      <UpcomingWebinars/>
+      <Instructors/>
+      <Footer/>
     </main>
   );
 }
